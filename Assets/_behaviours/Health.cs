3981fa883e1e7277eed0ca633229e2c8421a950e
TomMulvaney <mulvaney.tom@gmail.com>
2015-01-22 22:02:55 +0000

    Art update. Created Health

---
+++ Assets/_behaviours/Health.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour 
+{
+    [SerializeField]
+    private int m_maxHealth = 100;
+    [SerializeField]
+    UISlider m_slider;
+
+    private int m_currentHealth;
+
+	// Use this for initialization
+	void Awake () 
+    {
+        m_currentHealth = m_maxHealth;
+	}
+
+    public void ChangeHealth(int delta)
+    {
+        m_currentHealth += delta;
+        m_slider.value = (float)m_currentHealth / (float)m_maxHealth;
+    }
+}
---
+++ Assets/_behaviours/Move/RotateFollower.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+// TODO:
+// 1. Rename
+// 2. SerializeField: m_transformMatch: up, down, forward, backward, left and right
+// 3. SerializeField: m_rotateSpeed = -1 (if -1 then instant)
+public class RotateFollower : MonoBehaviour 
+{	
+    Vector3 m_lastPos;
+
+	void Update () 
+    {
+        Vector3 velocity = transform.position - m_lastPos;
+
+        if (!MathHelpers.Approximately(velocity, Vector3.zero))
+        {
+            transform.up = velocity;
+        }
+
+        m_lastPos = transform.position;
+	}
+}
